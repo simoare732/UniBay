ba9ab71edb917cf707ff8f5dc315a694be852f46
Simone Aresta <63786933+simoare732@users.noreply.github.com>
2025-01-24 12:36:22 +0100

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'simoaresta3@gmail.com'     # Il tuo indirizzo Gmail
-EMAIL_HOST_PASSWORD = 'gjrr nofd lrxl efff'
+EMAIL_HOST_USER = 'admin@admin.com'     # Your email
+EMAIL_HOST_PASSWORD = ''                # Password
